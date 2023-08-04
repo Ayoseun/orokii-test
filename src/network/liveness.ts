976b0a45314cf7822d3a3cf09bdf4a8a4f47bef1
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2023-08-04 06:49:36 +0100

    up

--- src/network/liveness.ts
+++ src/network/liveness.ts
@@ -1,30 +1,27 @@
-
-
 export default async function livenessCheck(token: any) {
-  var res:any={}
-  console.log(token)
+  var res:any = {};
+  console.log(token);
   try {
-   
-    const url = 'http://35.232.94.217:5800/api/liveness';
-    const data = { tokenImage: token }; // Replace with your request payload
+    const url = 'https://facephi.orokii.com/api/selphid/passive-liveness/evaluate/token';
+    const data = { "tokenImage": token }; // Replace with your request payload
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://orokii-test.vercel.app' // Replace with the allowed origin
       },
       body: JSON.stringify(data)
     });
     console.log('Data:', response);
     if (response.ok) {
       const responseData = await response.json();
-      res=responseData;
+      res = responseData;
       console.log('Data:', responseData);
     } else {
       console.log('Request failed with status:', response.status);
-
     }
   } catch (error) {
-
+    // Handle error here
   }
-  return res
+  return res;
 }
--- src/network/matching.ts
+++ src/network/matching.ts
@@ -10,7 +10,7 @@ export default async function face_IDMatchingData(doc: any) {
   try {
     let facetemplateRaw = localStorage.getItem("imageTemplateRaw");
 
-    const url = 'https://facephi.orokii.com/api/selphid/authenticate-facial/image/template';
+    const url = 'http://35.232.94.217:5800/api/facedoc';
     const data ={
       "image1": doc,
       "faceTemplate1": facetemplateRaw
